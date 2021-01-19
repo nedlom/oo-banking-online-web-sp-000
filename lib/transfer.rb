1923b595da28b4c6f229e58443ce28f94d4a64c9
imperative-scientist-4600 <nelom3@gmail.com>
2021-01-19 20:53:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    
+    if sender.valid? 
     if status == "pending" && sender.balance > amount
       sender.deposit(-amount)
       receiver.deposit(amount)
