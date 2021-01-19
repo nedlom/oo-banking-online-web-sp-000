b47814886f7f099558a311c1fbd8778d7bd16c8e
imperative-scientist-4600 <nelom3@gmail.com>
2021-01-19 21:26:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,38 +32,3 @@ class Transfer
     end
   end
 end
-
-# class Transfer
-  
-#   attr_accessor :sender, :receiver, :status, :amount
-  
-#   def initialize(sender, receiver, amount)
-#     @sender = sender
-#     @receiver = receiver
-#     @status = "pending"
-#     @amount = amount
-#   end
-  
-#   def valid?
-#     sender.valid? && receiver.valid?
-#   end
-  
-#   def execute_transaction
-#     if status == "pending" && valid? && sender.balance >= amount
-#       sender.deposit(-amount)
-#       receiver.deposit(amount)
-#       self.status = "complete"
-#     else
-#       self.status = "rejected"
-#       "Transaction rejected. Please check your account balance."
-#     end
-#   end
-  
-#   def reverse_transfer
-#     if status == "complete"
-#       sender.deposit(amount)
-#       receiver.deposit(-amount)
-#       self.status = "reversed"
-#     end
-#   end
-# end
